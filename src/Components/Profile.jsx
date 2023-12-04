15c826c58b43bfe81d1253de31b360f94e167dff
yeonna18k <yeonna18k@gmail.com>
2023-12-04 10:39:33 +0900

    final

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -10,32 +10,45 @@ function Profile() {
   const dispatch = useDispatch();
   const auth = useSelector((state) => state.auth);
 
+  const [img, setImg] = useState(auth.avatar);
   const [uploadImg, setUploadImg] = useState();
+  const [isEditing, setIsEditing] = useState(false);
 
-  // useEffect(() => {
-  //   dispatch(setAvatar(localStorage.getItem("avatar")));
-  // }, []);
+  const [editNickname, setEditNickname] = useState(auth.nickname);
+
   const imgChangeHandler = async (e) => {
     const file = e.target.files[0];
     const fileReader = new FileReader();
     fileReader.readAsDataURL(file);
     fileReader.onload = (data) => {
-      setUploadImg(file);
+      setImg(data.target.result);
     };
+    setUploadImg(file);
   };
 
   const newImgHandler = async () => {
-    const formData = new FormData();
-    formData.append("avatar", uploadImg);
-    const { data } = await api.patch("/profile", formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-      },
-    });
-    // console.log(data);
-    dispatch(setAvatar(data));
-    localStorage.setItem("avatar", data.avatar);
+    try {
+      const formData = new FormData();
+      formData.append("avatar", uploadImg);
+      const { data } = await api.patch("/profile", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+      console.log(data);
+      dispatch(setAvatar(data));
+      localStorage.setItem("avatar", data.avatar);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsEditing(false);
+  };
+  const onClickEdit = () => {
+    setIsEditing(true);
+  };
+  const onClickCancel = () => {
+    setIsEditing(false);
   };
 
   return (
@@ -45,16 +58,36 @@ function Profile() {
           <ProfileTitle>Edit Profile</ProfileTitle>
           <label>
             <ProfileFigure>
-              <img src={localStorage.getItem("avatar")} alt="아바타이미지" />
+              <img
+                src={img ?? localStorage.getItem("avatar")}
+                alt="아바타이미지"
+              />
             </ProfileFigure>
             <input type="file" accept="image/*" onChange={imgChangeHandler} />
           </label>
-          <ProfileNickname>{auth.nickname}</ProfileNickname>
+          {isEditing === false ? (
+            <ProfileNickname>{auth.nickname}</ProfileNickname>
+          ) : (
+            <ProfileEditInput>
+              <input
+                type="text"
+                placeholder="auth.nickname"
+                value={editNickname}
+                onChange={(e) => setEditNickname(e.target.value)}
+              />{" "}
+            </ProfileEditInput>
+          )}
+
           <ProfileUserId>{auth.userId}</ProfileUserId>
           <ProfileButton>
-            <button onClick={newImgHandler}>Edit</button>
-            {/* <button>Cancel</button>
-            <button>Done</button> */}
+            {isEditing === false ? (
+              <button onClick={onClickEdit}>Edit</button>
+            ) : (
+              <>
+                <button onClick={onClickCancel}>Cancel</button>
+                <button onClick={newImgHandler}>Done</button>
+              </>
+            )}
           </ProfileButton>
         </ProfileBox>
       </Container>
@@ -96,11 +129,19 @@ const ProfileNickname = styled.span`
   font-size: 24px;
   font-weight: bolder;
 `;
+const ProfileEditInput = styled.div`
+  border: 1px solid white;
+
+  height: 36px;
+  width: 200px;
+  margin: 0 auto;
+`;
 const ProfileUserId = styled.span``;
 const ProfileButton = styled.div`
   & button {
     border: 1px solid white;
     padding: 5px 15px;
+    margin: 10px;
     background-color: transparent;
     cursor: pointer;
   }
